ac53f0bcd96005a3bfb47a5e763ed2e2ed7d9b56
andreas <andreas@immerda.ch>
2009-02-11 12:59:00 +0000

    postgres: allow user login privileges; (enforce encrypted pw store)

--- manifests/role.pp
+++ manifests/role.pp
@@ -1,13 +1,13 @@
 define postgres::role($ensure, $password = false) {
     $passtext = $password ? {
         false => "",
-        default => "PASSWORD '$password'"
+        default => "ENCRYPTED PASSWORD '$password'"
     }
     case $ensure {
         present: {
             # The createuser command always prompts for the password.
             exec { "Create $name postgres role":
-                command => "/usr/bin/psql -c \"CREATE ROLE $name $passtext\"",
+                command => "/usr/bin/psql -c \"CREATE ROLE $name WITH LOGIN $passtext\"",
                 user => "postgres",
                 unless => "/usr/bin/psql -c '\\du' | grep '^  *$name'"
             }
