4e9378a07d490cb77a736da7524b26fd8f55d80a
mh <mh@immerda.ch>
2009-10-03 15:32:43 +0200

    add out rules

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,6 +1,9 @@
 class postgres::client {
-   include ruby::postgres
-   package{'postgresql': 
-      ensure => installed,
-   }
+  include ruby::postgres
+  package{'postgresql': 
+    ensure => installed,
+  }
+  if $use_shorewall {
+    include shorewall::rules::out::postgres
+  }
 }
