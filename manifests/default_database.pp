598f53f9abf166fc2c88fad5f3f05fb0af6c3311
mh <mh@immerda.ch>
2011-03-18 11:02:46 +0100

    add a define to create a standardized database/owner

---
+++ manifests/default_database.pp
@@ -0,0 +1,36 @@
+# create a database with an owner and optionally create that owner
+# encrypt password like
+# ruby -r'digest/md5' -e 'puts "md5" + Digest::MD5.hexdigest(ARGV[1] + ARGV[0])' USERNAME PASSWORD
+define postgres::default_database(
+  $username = 'absent',
+  $password,
+  $ensure = 'present'
+) {
+  case $username {
+    'absent': { $owner = $name }
+    default: { $owner = $username }
+  }
+  case $password {
+    'absent': { info("we don't create the user for database: ${name}") }
+    default: {
+      postgres::role{
+        $owner:
+          password => $password,
+          ensure => $ensure;
+      }
+    }
+  }
+  postgres::database{
+    $name:
+      ensure => $ensure,
+      owner => $owner;
+  }
+  case $password {
+    'absent': {}
+    default: {
+      Postgres::Database[$name] {
+        require => Postgres::Role[$owner]
+      }
+    }
+  }
+}
