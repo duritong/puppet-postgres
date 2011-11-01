9183e442e1e58d3a84ddefd98628eb9d36d7f4d3
mh <mh@immerda.ch>
2011-11-01 08:30:32 +0100

    introduce trocla to the role

--- manifests/role.pp
+++ manifests/role.pp
@@ -5,30 +5,34 @@ define postgres::role(
 ) {
   require ::postgres
   require ::postgres::client
-  $passtext = $password ? {
+  $real_password = $password ? {
+    'trocla' => trocla("postgres_${name}",'pgsql',"username: ${name}"),
+    default => $password,
+  }
+  $passtext = $real_password ? {
     false => "",
-    default => "ENCRYPTED PASSWORD '$password'"
+    default => "ENCRYPTED PASSWORD '${real_password}'"
   }
   case $ensure {
     present: {
       # The createuser command always prompts for the password.
       exec { "Create $name postgres role":
-        command => "/usr/bin/psql -c \"CREATE ROLE $name $options $passtext LOGIN\"",
+        command => "/usr/bin/psql -c \"CREATE ROLE ${name} ${options} ${passtext} LOGIN\"",
         user => "postgres",
         unless => "/usr/bin/psql -c '\\du' | grep '^  *$name'",
         require => Service['postgresql'],
       }
     }
     absent:  {
-      exec { "Remove $name postgres role":
-        command => "/usr/bin/dropuser $name",
+      exec { "Remove ${name} postgres role":
+        command => "/usr/bin/dropuser ${name}",
         user => "postgres",
-        onlyif => "/usr/bin/psql -c '\\du' | grep '$name  *|'",
+        onlyif => "/usr/bin/psql -c '\\du' | grep '${name}  *|'",
         require => Service['postgresql'],
       }
     }
     default: {
-      fail "Invalid 'ensure' value '$ensure' for postgres::role"
+      fail "Invalid 'ensure' value '${ensure}' for postgres::role"
     }
   }
 }
