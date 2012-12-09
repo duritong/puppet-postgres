cfeb6264a8102e99beca9a36496244ef95206350
mh <mh@immerda.ch>
2012-12-09 15:38:29 +0100

    linting

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,3 +1,4 @@
+# setup the common things for postgres
 class postgres::base {
   class{'postgres::client':
     manage_shorewall => $postgres::manage_shorewall
@@ -5,58 +6,65 @@ class postgres::base {
 
   package { 'postgresql-server':
     ensure => present,
-  }
-
-  service{'postgresql':
-    enable => true,
-    ensure => running,
+  } -> service{'postgresql':
+    ensure    => running,
+    enable    => true,
     hasstatus => true,
-    require => Package['postgresql-server'],
   }
 
   exec{'service postgresql initdb':
-      unless => 'test -f /var/lib/pgsql/data/postgresql.conf',
-      require => Package['postgresql-server'],
-      before => File['/var/lib/pgsql/data/pg_hba.conf','/var/lib/pgsql/data/postgresql.conf'],
+    unless  => 'test -f /var/lib/pgsql/data/postgresql.conf',
+    require => Package['postgresql-server'],
+    before  => File['/var/lib/pgsql/data/pg_hba.conf','/var/lib/pgsql/data/postgresql.conf'],
   }
 
   # wen want to be sure that this exists
   file{
-    '/var/lib/pgsql/backups':
-      ensure => directory,
-      require => Package['postgresql-server'],
-      owner => postgres, group => postgres, mode => 0700;
-    '/etc/cron.d/pgsql_backup.cron':
-      source => "puppet:///modules/postgres/backup/pgsql_backup.cron",
-      require => File['/var/lib/pgsql/backups'],
-      owner => root, group => 0, mode => 0600;
-    '/etc/cron.d/pgsql_vacuum.cron':
-      source => "puppet:///modules/postgres/maintenance/pgsql_vacuum.cron",
-      owner => root, group => 0, mode => 0600;
-    '/var/lib/pgsql/data/pg_hba.conf':
-      source => [
-        "puppet:///modules/site_postgres/${::fqdn}/pg_hba.conf",
-        "puppet:///modules/site_postgres/pg_hba.conf",
-        "puppet:///modules/postgres/config/pg_hba.conf.${::operatingsystem}",
-        "puppet:///modules/postgres/config/pg_hba.conf"
-      ],
-      ensure => file,
-      require => Package['postgresql-server'],
-      notify => Service['postgresql'],
-      owner => postgres, group => postgres, mode => 0600;
-    '/var/lib/pgsql/data/postgresql.conf':
-      source => [
-        "puppet:///modules/site_postgres/${::fqdn}/postgresql.conf",
-        "puppet:///modules/site_postgres/postgresql.conf",
-        "puppet:///modules/postgres/config/postgresql.conf.${::operatingsystem}",
-        "puppet:///modules/postgres/config/postgresql.conf"
-      ],
-      ensure => file,
-      require => Package['postgresql-server'],
-      notify => Service['postgresql'],
-      owner => postgres, group => postgres, mode => 0600;
-    '/usr/local/bin/readonly_for_pgsql.sh':
-      source => 'puppet:///modules/postgres/maintenance/readonly_for_pgsql.sh',
-      owner => postgres, group => postgres, mode => 0500;
+  '/var/lib/pgsql/backups':
+    ensure  => directory,
+    require => Package['postgresql-server'],
+    owner   => postgres,
+    group   => postgres,
+    mode    => '0700';
+  '/etc/cron.d/pgsql_backup.cron':
+    source  => 'puppet:///modules/postgres/backup/pgsql_backup.cron',
+    require => File['/var/lib/pgsql/backups'],
+    owner   => root,
+    group   => 0,
+    mode    => '0600';
+  '/etc/cron.d/pgsql_vacuum.cron':
+    source  => 'puppet:///modules/postgres/maintenance/pgsql_vacuum.cron',
+    owner   => root,
+    group   => 0,
+    mode    => '0600';
+  '/var/lib/pgsql/data/pg_hba.conf':
+    source  => [
+      "puppet:///modules/site_postgres/${::fqdn}/pg_hba.conf",
+      'puppet:///modules/site_postgres/pg_hba.conf',
+      "puppet:///modules/postgres/config/pg_hba.conf.${::operatingsystem}",
+      'puppet:///modules/postgres/config/pg_hba.conf'
+    ],
+    require => Package['postgresql-server'],
+    notify  => Service['postgresql'],
+    owner   => postgres,
+    group   => postgres,
+    mode    => '0600';
+  '/var/lib/pgsql/data/postgresql.conf':
+    source  => [
+      "puppet:///modules/site_postgres/${::fqdn}/postgresql.conf",
+      'puppet:///modules/site_postgres/postgresql.conf',
+      "puppet:///modules/postgres/config/postgresql.conf.${::operatingsystem}",
+      'puppet:///modules/postgres/config/postgresql.conf'
+    ],
+    require => Package['postgresql-server'],
+    notify  => Service['postgresql'],
+    owner   => postgres,
+    group   => postgres,
+    mode    => '0600';
+  '/usr/local/bin/readonly_for_pgsql.sh':
+    source  => 'puppet:///modules/postgres/maintenance/readonly_for_pgsql.sh',
+    owner   => postgres,
+    group   => postgres,
+    mode    => '0500';
   }
 }
